2150e85ce7cff231fc007bd08d364899ab6b77a2
Kevin <kevin005005@gmail.com>
2021-03-13 21:13:00 +0100

    Statistics are now dynamically loaded.

--- Processor.py
+++ Processor.py
@@ -1,9 +1,20 @@
 from classes.Person import Person
+from datetime import datetime
+import os
+from bs4 import BeautifulSoup
+import requests
+
+DATA_FOLDER = 'data'
+TXT_EXTENSION = '.txt'
+
 
 # Based on data from March 11
-number_of_infections = 489172
-number_of_deaths = 16479
-number_of_recoveries = 344267
+
+def today():
+    return "{0}-{1}-{2}".format(datetime.now().year, datetime.now().month, datetime.now().day)
+
+
+STATISTICS = os.path.join(DATA_FOLDER, 'Statistics (' + today() + ')' + TXT_EXTENSION)
 
 
 def get_value_from_block(value: str, block: str):
@@ -18,6 +29,24 @@ def percentage_calculator(arr1, arr2, ratio_only=False):
         return ratio * 100
 
 
+try:
+    with open(STATISTICS) as file:
+        number_of_infections = int(file.readline().replace(',', ''))
+        number_of_deaths = int(file.readline().replace(',', ''))
+        number_of_recoveries = int(file.readline().replace(',', ''))
+except FileNotFoundError:
+    with open(STATISTICS, 'w') as file:
+        worldometer = BeautifulSoup(requests.get('https://www.worldometers.info/coronavirus/country/hungary/').text,
+                                    'html.parser')
+        elements = worldometer.findAll('div', {'class': 'maincounter-number'})
+        statistics = [e.text.strip().replace(',', '') for e in elements]
+        number_of_infections = int(statistics[0])
+        number_of_deaths = int(statistics[1])
+        number_of_recoveries = int(statistics[2])
+        file.write('\n'.join(statistics))
+
+print(today() + TXT_EXTENSION)
+
 data = open('data/data.txt', "r", encoding='utf-8').read()
 
 people = []
@@ -30,9 +59,12 @@ for block in data.split("###"):
     people.append(Person(number, sex, age, chronic_illnesses))
 
 print('0-17: {:.2f}%'.format(percentage_calculator([person.age for person in people if person.age < 18], people)))
-print('18-44: {:.2f}%'.format(percentage_calculator([person.age for person in people if 18 <= person.age < 45], people)))
-print('45-64: {:.2f}%'.format(percentage_calculator([person.age for person in people if 45 <= person.age < 65], people)))
-print('65-74: {:.2f}%'.format(percentage_calculator([person.age for person in people if 65 <= person.age < 75], people)))
+print(
+    '18-44: {:.2f}%'.format(percentage_calculator([person.age for person in people if 18 <= person.age < 45], people)))
+print(
+    '45-64: {:.2f}%'.format(percentage_calculator([person.age for person in people if 45 <= person.age < 65], people)))
+print(
+    '65-74: {:.2f}%'.format(percentage_calculator([person.age for person in people if 65 <= person.age < 75], people)))
 print('75+: {:.2f}%'.format(percentage_calculator([person.age for person in people if 75 <= person.age], people)))
 
 print('\n')
